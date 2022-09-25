501ff68c6dda2616c2b5eac711432b85184a81bc
ngacho <brandonngacho@gmail.com>
2022-09-25 16:59:38 -0400

    activation function

--- tf-tutorial-mlm/code_samples/backprop_neural_net.py
+++ tf-tutorial-mlm/code_samples/backprop_neural_net.py
@@ -8,13 +8,16 @@ def initialize_network(n_inputs, n_hidden, n_outputs):
     # add 1 to get n_input weights + a bias
     hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
     network.append(hidden_layer)
-    # ouput layer has n_output layers,  each one with 1 weight and bias.
+    # ouput layer has n_output layers,  each one with n_hidden layer weights and bias.
     output_layer = [{'weights':[random() for i in range(n_hidden+1)]} for i in range(n_outputs)]
     network.append(output_layer)
     return network
 
+# calculate neuron activation for an input
+def activate(weights, inputs):
+    activation = weights[-1]
+    for(i in range(len(weights)-1)):
+        activation += weights[i] * inputs[i]
+    return activation.
 
-seed(1)
-network = initialize_network(2,1,2)
-for layer in network:
-    print(layer)
+
